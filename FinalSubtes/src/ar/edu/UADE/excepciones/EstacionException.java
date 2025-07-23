d5f87c87ca6ef7858fe28d896e7ec79c462693a1
Bernardo Olmedo <bolmedo@uade.edu.ar>
2025-07-23 14:20:45 -0300

    Delete FinalSubtes/src/ar/edu/UADE directory

--- FinalSubtes/src/ar/edu/UADE/excepciones/EstacionException.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.UADE.excepciones;
-
-public class EstacionException extends Exception{
-	
-	public EstacionException(String mensage) {
-		super(mensage);
-	}
-
-}
--- FinalSubtes/src/ar/edu/UADE/excepciones/LineaException.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.UADE.excepciones;
-
-public class LineaException extends Exception{
-
-	public LineaException(String mensaje) {
-		super(mensaje);
-	}
-	
-}
